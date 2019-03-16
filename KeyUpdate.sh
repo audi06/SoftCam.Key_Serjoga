2458aa104a4c0b96b2a859955d08921b68849525
audi06 <audi06_19@hotmail.com>
2019-03-16 12:01:31 +0300

    Update KeyUpdate.sh

--- KeyUpdate.sh
+++ KeyUpdate.sh
@@ -32,11 +32,11 @@ else
   echo "It was sent to the following folder."
   echo ": /etc/tuxbox/config/"
   echo ": /usr/keys/"
-  cp -f /usr/keys/SoftCam.Key /etc/tuxbox/config/SoftCam.Key
+  check="/etc/tuxbox/config/SoftCam.Key";
 #  exit 0;
 fi;
 # copy
-cp -f /usr/keys/SoftCam.Key $check;
+cp -rd /usr/keys/SoftCam.Key $check;
 echo "SoftCam.Key file sent. $check;";
 echo "";
 echo "SoftCam.Key updated. Do not forget to restart the EMU. good looking ...";
